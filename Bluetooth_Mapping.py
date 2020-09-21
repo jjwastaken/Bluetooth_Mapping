defe385099f2d923db72c872820c78ae6b81c110
jjwastaken <45520490+jjwastaken@users.noreply.github.com>
2020-09-20 21:24:48 -0700

    Update Bluetooth_Mapping.py

--- Bluetooth_Mapping.py
+++ Bluetooth_Mapping.py
@@ -40,36 +40,39 @@ def main():
          ultra1 = (IncomingData1).decode('utf-8')
          print(ultra1)
          distance = ultra1.rstrip().split(",")
-         print(distance[3])
 
       #IncomingData2=SerialPort.readline()
       #if(IncomingData2):
       #   ultra2 = (IncomingData2).decode('utf-8')
       #   print(ultra2)
-  
-      # if the sensors are equal with uncertainty of 5cm
+
+      ###############################################################
+      # distance[0] is the distance of distanceUpLeft
+      # distance[1] is the distance of distanceUpright and so on (accordingly with the Arduino Serial.print)
+      # when you use distance[?], you need to convert to float with float(distance[?])
+      ###############################################################
 
       if(IncomingData1):
+         # if the sensors are equal with uncertainty of 2cm
          if float(distance[0]) < float(distance[1]) + 0.02 and float(distance[0]) > float(distance[1]) - 0.02:
-            if float(distance[0]) < 0.1 and float(distance[1]) < 0.1:
+            # if the UpLeft and UpRight sensors are within 10cm with an obstacle
+            if float(distance[0]) < 0.1 and float(distance[1]) < 0.1: # it is a wall, append the x,y coordinates
                print('wall')
-               if float(distance[2]) < float(distance[3]):
+               if float(distance[2]) < float(distance[3]): # might need trial & error for plotting
                   xplot.append(float(distance[2]))
                   yplot.append(float(1.5 - float(distance[0])))
                else:
                   xplot.append(float(distance[3]))
                   yplot.append(float(distance[0]))
-               # it is a wall, paint the canvas white
-            elif float(distance[0]) < float(distance[1]): # if left scanned an obstacle
-               print('left')
+            elif float(distance[0]) < float(distance[1]): # if UpLeft scanned an obstacle, append x,y coordinate
+               print('UpLeft')
                yplot.append(float(distance[1]))
-               if float(distance[2]) < float(distance[3]):
+               if float(distance[2]) < float(distance[3]): # trial & error for plotting
                   xplot.append(float(distance[2]))
                else:
                   xplot.append(float(distance[3]))
-               # paint canvas white on the left side
             elif float(distance[0]) > float(distance[1]): # if right scanned an obstacle
-               print('right')
+               print('UpRight')
                yplot.append(float(distance[0]))
                if float(distance[2]) < float(distance[3]):
                   xplot.append(float(distance[2]))
