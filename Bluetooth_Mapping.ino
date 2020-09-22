5258bbbafa045d286d7f556c6b592060e4e28e4f
benh4627 <43359178+benh4627@users.noreply.github.com>
2020-09-21 17:04:43 -0700

    Update Bluetooth_Mapping.ino

--- Bluetooth_Mapping.ino
+++ Bluetooth_Mapping.ino
@@ -1,12 +1,19 @@
 char Incoming_value = 0;   
 const int trigPin1 = 12;
 const int echoPin1 = 13; 
-const int trigPin2 = 10;
-const int echoPin2 = 11;
-const int trigPin3 = 2;
-const int echoPin3 = 3;
-const int trigPin4 = 4;
-const int echoPin4 = 5;
+const int trigPin2 = 2;
+const int echoPin2 = 3;
+const int trigPin3 = 4;
+const int echoPin3 = 5;
+const int trigPin4 = 10;
+const int echoPin4 = 11;
+
+//MOTOR 1
+const int pin9 = 9;
+const int pin8 = 8;
+//MOTOR 2
+const int pin7 = 7;
+const int pin6 = 6;
 
 void setup() 
 { 
@@ -18,7 +25,12 @@ void setup()
   pinMode(trigPin3, OUTPUT);
   pinMode(echoPin3, INPUT);  
   pinMode(trigPin4, OUTPUT);
-  pinMode(echoPin4, INPUT);   
+  pinMode(echoPin4, INPUT); 
+      // set all the motor control pins to outputs
+  pinMode(pin9, OUTPUT);
+  pinMode(pin8, OUTPUT);
+  pinMode(pin7, OUTPUT);
+  pinMode(pin6, OUTPUT);  
 }
 
 double ultraSonic(int trig, int ech)
@@ -33,10 +45,52 @@ double ultraSonic(int trig, int ech)
     return distance;
 }
 
+void moveForward(int seconds) // move forward in seconds
+{
+  digitalWrite(pin9, LOW);
+  digitalWrite(pin8, HIGH);  
+  digitalWrite(pin7, LOW);
+  digitalWrite(pin6, HIGH); 
+  delay(seconds * 1000);
+}
+
+void moveBackward(int seconds) // move backward in seconds
+{
+  digitalWrite(pin9, HIGH);
+  digitalWrite(pin8, LOW);
+  digitalWrite(pin7, HIGH);
+  digitalWrite(pin6, LOW);
+  delay(seconds * 1000);
+}
+
+void turnRight(int seconds) {
+  digitalWrite(pin9, HIGH);
+  digitalWrite(pin8, LOW);
+  digitalWrite(pin7, LOW);
+  digitalWrite(pin6, HIGH);
+  delay(seconds * 1000);
+}
+
+void turnLeft(double seconds) {
+  digitalWrite(pin9, LOW);
+  digitalWrite(pin8, HIGH);
+  digitalWrite(pin7, HIGH);
+  digitalWrite(pin6, LOW);
+  delay(seconds * 1000);
+}
+
+void stopp()
+{
+  digitalWrite(pin9, LOW);
+  digitalWrite(pin8, LOW);
+  digitalWrite(pin7, LOW);
+  digitalWrite(pin6, LOW);
+}
+
+const int avoidDistance = 0.8; // 10cm avoid
 void loop()
 {
-  if(Serial.available() > 0) 
-  {
+    moveForward(1);
       char cmd = Serial.read();
       double distanceUpLeft = ultraSonic(trigPin1, echoPin1);
       double distanceUpRight = ultraSonic(trigPin2, echoPin2);
@@ -63,18 +117,13 @@ void loop()
         //Serial.println(distanceRight);
         //delay(20);
       }
+        if(distanceUpLeft + 0.05 < distanceUpRight && distanceUpRight < 10) // if left is blocked
+        {
+          turnRight(1);
+        }
+        else if(distanceUpLeft - 0.05 > distanceUpRight && distanceUpRight < 10)
+        {
+          turnLeft(1);
+        }
       
-      const int avoidDistance = 0.1; // 10cm avoid
-      if(distanceUpLeft < avoidDistance || distanceUpRight < avoidDistance)
-      {
-        if(distanceUpLeft < distanceUpRight) // if left is blocked
-        {
-          // turn right to avoid
-        }
-        else if(distanceUpLeft > distanceUpRight)
-        {
-          // turn left to avoid
-        }
-      }
-  }
 }
